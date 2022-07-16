b5eccd018e357a63e50c90631da88a386bf84a86
Marina <gtellier.mecanics@gmail.com>
2022-07-16 17:13:19 +0300

    Количество чисел больше 0

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите количество чисел");
+int count = Convert.ToInt32(Console.ReadLine());
+PositiveNumbers(count);
+
+void PositiveNumbers(int cou)
+{
+    int positiveCount = 0;
+    Console.WriteLine("Введите числа");
+    for (int i = 0; i < cou; i++)
+    {
+       int number = Convert.ToInt32(Console.ReadLine());
+       if (number > 0)
+        {
+            positiveCount += 1;
+        }
+    }
+    Console.WriteLine("Количество положительных чисел равно: " + positiveCount);
+}
